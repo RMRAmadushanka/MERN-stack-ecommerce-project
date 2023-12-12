fbc91e75c43d91d498e6e6781117e789ae819a6f
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-12 14:05:50 +0530

    Added Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Container from 'react-bootstrap/esm/Container'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <h1>Welcome to gaming</h1>
         </Container>
       </main>
+      <Footer/>
     </>
   )
 }
