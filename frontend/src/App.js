7748c2d010ece493fbe19a32955a6ade3f521cd1
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-12 14:43:17 +0530

    Add product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Container from 'react-bootstrap/esm/Container'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <h1>Welcome to gaming</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer/>
