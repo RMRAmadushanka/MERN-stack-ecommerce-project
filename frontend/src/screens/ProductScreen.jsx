74c56d4c6584e953c946d6e7f49be18353618612
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-14 11:21:10 +0530

    connected backend api

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products'
+
 import { Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
+import axios from 'axios'
 const ProductScreen = () => {
+    const [product, setProduct] = useState([]);
     const {id: productId} = useParams();
-    const product = products.find((p)=> p._id === productId);
+
+
+    useEffect(()=>{
+        const fetchProducts= async () =>{
+            const {data} = await axios.get(`http://localhost:5000/api/products/${productId}`);
+            setProduct(data)
+        }
+
+        fetchProducts()
+    },[productId])
 
   return (
     <>
