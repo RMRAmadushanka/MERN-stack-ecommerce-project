f5ce523ae72bd19a141a93aa211b0520d07aca8e
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-16 20:17:08 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 dotenv.config();
 const port  = process.env.PORT;
+import { notFound,errorHandler } from './middleware/errorHandler.js';
 
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
@@ -10,6 +11,8 @@ connectDB();
 const app = express();
 app.use(cors())
 app.use('/api/products',productRoutes)
+app.use(notFound)
+app.use(errorHandler);
 
 app.listen(port,() => {
     console.log(`Server running on ${port}`);
