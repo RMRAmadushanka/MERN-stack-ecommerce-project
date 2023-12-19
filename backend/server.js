51bb35d8539375fe38c04286f4e2858ba90cd8e6
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-19 23:35:13 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
-const port  = process.env.PORT;
-import { notFound,errorHandler } from './middleware/errorHandler.js';
+const port = process.env.PORT;
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
-import productRoutes from './routes/productRoutes.js'
-import cors from 'cors'
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 connectDB();
 const app = express();
-app.use(cors())
-app.use('/api/products',productRoutes)
-app.use(notFound)
+app.use(cors());
+app.use("/api/products", productRoutes);
+app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port,() => {
-    console.log(`Server running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
