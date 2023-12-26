46b42510f72c5ec44d62273dcb7b7d82fd7b90d8
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-26 13:18:08 +0530

    Add cart dynamic cart icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,52 +1,64 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import{ FaShoppingCart, FaUser} from 'react-icons/fa'
-import Nav from 'react-bootstrap/Nav';
-import { LinkContainer} from 'react-router-bootstrap'
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Badge } from "react-bootstrap";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
-      <Navbar className="bg-body-tertiary" expand='md' collapseOnSelect>
+      <Navbar className="bg-body-tertiary" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-          <Navbar.Brand>
-            <img
-              alt=""
-              src='/images/brandLogo.png'
-              width="100"
-              height="50"
-              className="d-inline-block align-top"
-            />{' '}
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                alt=""
+                src="/images/brandLogo.png"
+                width="100"
+                height="50"
+                className="d-inline-block align-top"
+              />{" "}
+            </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-          <LinkContainer to='/'>
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/'>
-            <Nav.Link >Link</Nav.Link>
-            </LinkContainer>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link>
+                  Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
