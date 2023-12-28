6044af6b285299caa57446ab1e029d211072daa9
RMRAmadushanka <it19147260@my.sliit.lk>
2023-12-28 12:09:21 +0530

    code indentation fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,22 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import { authUser, deleteUser, getUserById,getUserProfile,getUsers, logoutUser, registerUser, updateUser, updateUserProfile } from '../controllers/userController';
+import {
+  authUser,
+  deleteUser,
+  getUserById,
+  getUserProfile,
+  getUsers,
+  logoutUser,
+  registerUser,
+  updateUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
-router.route('/').post(registerUser).get(getUsers);
-router.route('/logout', logoutUser);
-router.route('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser)
+router.route("/").post(registerUser).get(getUsers);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
 
-export default router; 
+export default router;
