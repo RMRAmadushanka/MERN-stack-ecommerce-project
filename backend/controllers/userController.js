1012feb417fc8d7493a71d59be49d3eccae2577d
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-01 11:05:49 +0530

    Added User login using email and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,13 +9,16 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (user) {
-    res.json({
+  if (user && (await user.matchPassword(password))) {
+    res.json ({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user,
+      isAdmin: user.isAdmin,
     });
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ import userRoutes from "./routes/userRoutes.js"
 import cors from "cors";
 connectDB();
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(cors());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
