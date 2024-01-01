d028b831d3de6abc385dd5efcecbc0ee54ed609d
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-01 15:17:39 +0530

    Added create auth JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,36 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-
-//@desc auth user
-//@route POST /api/user/login
-//@access Public
+import jwt from "jsonwebtoken";
+// @desc Authenticate user and get token
+// @route POST/api/users/login
+// @access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.json ({
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "20d", //expires in 20 days
+    });
+
+    //set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 20 * 24 * 60 * 1000, // 20 days n
+    });
+
+    res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
     });
   } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
+    res.status(401);
+    throw new Error("Invalid email or password");
   }
 });
 
