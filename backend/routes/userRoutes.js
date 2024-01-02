15b536668051ea5cdbf64759e7516829b554ee3b
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-02 15:33:40 +0530

    Added route middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,10 +13,12 @@ import {
   updateUserProfile,
 } from "../controllers/userController.js";
 
-router.route("/").post(registerUser).get(getUsers);
+import { admin, protect } from "../middleware/authmiddleware.js";
+
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/logout", logoutUser);
 router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin,deleteUser).get(protect, admin,getUserById).put(protect, admin,updateUser);
 
 export default router;
