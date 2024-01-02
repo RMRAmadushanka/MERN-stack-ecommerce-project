82343aea8c0d2b7c9afd9e5c8cd1fa40141c752c
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-02 22:59:27 +0530

    Added user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 // @desc Authenticate user and get token
 // @route POST/api/users/login
 // @access Public
@@ -10,17 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "20d", //expires in 20 days
-    });
-
-    //set JWT as HTTP-Only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 20 * 24 * 60 * 1000, // 20 days n
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -38,7 +28,31 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/user
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //@desc  Logout user , clear cookie
