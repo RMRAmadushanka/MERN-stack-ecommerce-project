1812cdd02ae9062152c9676af1b99609f17561dd
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-02 15:33:56 +0530

    Added logout controller

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT;
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
 
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 connectDB();
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(cookieParser());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 
