fdccc33e0d2aac360cd7a15bdfe595e6787576fe
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-04 21:30:49 +0530

    Added get user profile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -70,7 +70,18 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@route GET /api/user/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isadmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 //@desc  Update user profile
