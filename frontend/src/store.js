fc102d3b825a9411c157618e815398d5d2183d7a
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-05 11:46:28 +0530

    Added user auth slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlices";
 
+
 import cartsliceReducer from './slices/cartSlice'
+import authSliceReducer from './slices/authSlice'
+
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath] : apiSlice.reducer,
-        cart: cartsliceReducer
+        cart: cartsliceReducer,
+        auth: authSliceReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
