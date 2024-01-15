c917063ca0bae8ab1fd3111774f0a074df051af5
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-15 22:51:58 +0530

    code indentation

--- frontend/src/screens/CheckoutSteps.jsx
+++ frontend/src/screens/CheckoutSteps.jsx
@@ -7,7 +7,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
     <Nav className="justify-content-center mb-4">
       <Nav.Item>
         {step1 ? (
-          <LinkContainer to='/login'>
+          <LinkContainer to="/login">
             <Nav.Link>Sign In</Nav.Link>
           </LinkContainer>
         ) : (
@@ -16,7 +16,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       </Nav.Item>
       <Nav.Item>
         {step2 ? (
-          <LinkContainer to='/shipping'>
+          <LinkContainer to="/shipping">
             <Nav.Link>Shipping</Nav.Link>
           </LinkContainer>
         ) : (
@@ -25,7 +25,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       </Nav.Item>
       <Nav.Item>
         {step3 ? (
-          <LinkContainer to='/payment'>
+          <LinkContainer to="/payment">
             <Nav.Link>Payment</Nav.Link>
           </LinkContainer>
         ) : (
@@ -34,7 +34,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       </Nav.Item>
       <Nav.Item>
         {step4 ? (
-          <LinkContainer tp='/placeorder'>
+          <LinkContainer tp="/placeorder">
             <Nav.Link>Place Order</Nav.Link>
           </LinkContainer>
         ) : (
