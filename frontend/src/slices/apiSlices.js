07f48e4f711eff6862c12b93d01896520e1d8743
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-19 21:11:34 +0530

    Added payment Screen

--- frontend/src/slices/apiSlices.js
+++ frontend/src/slices/apiSlices.js
@@ -6,6 +6,6 @@ const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
 
 export const apiSlice = createApi({
     baseQuery,
-    tasgTypes: ['Product', 'Order', 'User'],
+    tagsTypes: ['Product', 'Order', 'User'],
     endpoints: (builder) => ({})
 })
