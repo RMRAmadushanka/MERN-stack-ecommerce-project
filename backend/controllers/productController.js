2d459ce681ac90fa4be6709ffba691b0cadb269b
RMRAmadushanka <it19147260@my.sliit.lk>
2024-01-30 22:46:42 +0530

    Added product review backend route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,54 +37,83 @@ const createProduct = asyncHandler(async (req, res) => {
     numReviews: 0,
     description: "Sample description",
   });
-  const createProduct = await product.save()
-  res.status(201).json(createProduct)
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
 });
 
 //@desc Update the product
 //@route PUT /api/product/:id
 //@access Private
 const updateProduct = asyncHandler(async (req, res) => {
-  const {name,price, description, image, brand, category, countInStock } = req.body;
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
 
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
-  if(product) {
+  if (product) {
     product.name = name;
     product.price = price;
     product.description = description;
-    product.image  = image;
+    product.image = image;
     product.brand = brand;
     product.category = category;
-    product.countInStock = countInStock
+    product.countInStock = countInStock;
     const updateProduct = await product.save();
-  res.json(updateProduct)
+    res.json(updateProduct);
   } else {
-    res.status(404)
-    throw new Error('Resourse not found')
+    res.status(404);
+    throw new Error("Resourse not found");
   }
-
-  
 });
-
-
 
 //@desc Delete the product
 //@route DELETE /api/product/:id
 //@access Private
 const deleteProduct = asyncHandler(async (req, res) => {
-
-  const product = await Product.findById(req.params.id)
-
-  if(product) {
-    await Product.deleteOne({_id: product._id})
-    res.status(200).json({message:'Product deleted'})
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: "Product deleted" });
   } else {
-    res.status(404)
-    throw new Error('Resourse not found')
+    res.status(404);
+    throw new Error("Resourse not found");
   }
-
-  
 });
 
-export { deleteProduct, getProduct, getProducts, createProduct, updateProduct };
+//@desc create new reviwe
+//@route POST /api/product/:id/reviwes
+//@access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviews = product.reviews.find(
+      (review) => review.toString() == req.user._id.toString()
+    );
+    if (alreadyReviews) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(200).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Resourse not found");
+  }
+});
+
+export { deleteProduct, getProduct, getProducts, createProduct, updateProduct, createProductReview };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createProduct, deleteProduct, getProduct,getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, createProductReview, deleteProduct, getProduct,getProducts, updateProduct } from "../controllers/productController.js";
 const router = express.Router();
 
 
 router.route('/').get(getProducts).post(createProduct);
 router.route('/:id').get(getProduct).put(updateProduct).delete(deleteProduct);
-
+router.route('/:id/reviews').post(createProductReview)
 
 export default router;
